8581496d86b1e753a58f86a54d8ad21db02f8c0d
Anthony Nolletti <anthonynolletti@gmail.com>
2026-02-12 10:54:46 -0400

    Updates

--- js/language.js
+++ js/language.js
@@ -40,8 +40,7 @@ const I18N = {
     faq6_q: "Can students use FLORA at home with their families?",
     faq1_a: `FLORA is ready to go with built-in instructions and supports, learning activities and materials. <a href="https://nbed.sharepoint.com/:f:/r/sites/Test880/Shared%20Documents/1.1%20FLORA?csf=1&web=1&e=GZ5jpD" target="_blank">Select here</a> to access all resources.`,
     faq2_a: `The is created for <strong>K-4</strong> (primary/elementary) learners in mind. Instructions, recommended timing, and notes are written for teachers in the <a href="https://nbed.sharepoint.com/:f:/r/sites/Test880/Shared%20Documents/1.1%20FLORA/1.%20Quick%20Start%20Guide?csf=1&web=1&e=6gFP62" target="_blank">Quick Start Guide</a> so that the content stays joyful, engaging and effective for students and structured for you. For K-2 learners, FLORA aligns with <a href="https://curriculum.nbed.ca/learning-areas/primary-block/explore-your-world/" target="_blank">Explore Your World</a> curriculum.`,
-    faq4_a:
-      "Articulate is no longer supported, so you cannot download the FLORA modules. Please access the modules through the links provided on this site.",
+    faq4_a: `Articulate is no longer supported, so you cannot download the FLORA modules. Please access the <a href="#modules">modules</a> on this site.`,
     faq3_a: `Contact your district's <strong>FSL Subject Coordinator</strong> for pedagogical and resource support. Please make a <strong>helpdesk ticket</strong> through your district's IT support.`,
     faq6_a: `Of course! Strong home-school connections will support French language learning. Feel free to use this <strong><a href="https://nbed.sharepoint.com/:f:/r/sites/Test880/Shared%20Documents/1.1%20FLORA/3.%20Printable%20Resources?csf=1&web=1&e=kpK1cN" target="_blank">QR Code Take Home Printable</a></strong> so families can easily access FLORA at home.`,
     footer_org: "NB Department of Education and Early Childhood Development",
@@ -59,7 +58,7 @@ const I18N = {
     intro_title: "Qu’est-ce que FLORA?",
     intro_lead:
       "FLORA invite les élèves dans l’univers de Rigolo et Rigolette, où apprendre le français ressemble à un jeu et enseigner devient simple",
-    intro_pill: "Joyeux • Captivant • Efficace",
+    intro_pill: "Joyeux • Engageant • Efficace",
     get_started_title: `Commencez en 3 étapes... <i>allons-y!</i>`,
     step1_verb: "Lire",
     step1_link: "le guide de démarrage rapide",
@@ -87,9 +86,9 @@ const I18N = {
     faq3_q: "À qui puis-je adresser mes questions de soutien FLORA?",
     faq6_q: "Les élèves peuvent-ils utiliser FLORA à la maison?",
     faq1_a: `FLORA est prêt à utiliser grâce aux consignes intégrées, aux soutiens, aux activités et au matériel. <a href="https://nbed.sharepoint.com/:f:/r/sites/Test880/Shared%20Documents/1.1%20FLORA?csf=1&web=1&e=GZ5jpD" target="_blank">Cliquez ici</a> pour accéder à toutes les ressources.`,
-    faq2_a: `FLORA est conçu pour des élèves de <strong>M à 4e année</strong>. Les consignes, le temps recommandé et les notes sont rédigés pour le personnel enseignant dans le <a href="https://nbed.sharepoint.com/:f:/r/sites/Test880/Shared%20Documents/1.1%20FLORA/1.%20Quick%20Start%20Guide?csf=1&web=1&e=6gFP62" target="_blank">Guide de démarrage rapide</a> afin que le contenu demeure joyeux, captivant et efficace pour les élèves, tout en étant structuré pour vous. Pour les élèves de M à 2e année, FLORA s’aligne sur le programme <a href="https://curriculum.nbed.ca/learning-areas/primary-block/explore-your-world/" target="_blank">Explore Your World</a>.`,
+    faq2_a: `FLORA est conçu pour des élèves de <strong>M à 4e année</strong>. Les consignes, le temps recommandé et les notes sont rédigés pour le personnel enseignant dans le <a href="https://nbed.sharepoint.com/:f:/r/sites/Test880/Shared%20Documents/1.1%20FLORA/1.%20Quick%20Start%20Guide?csf=1&web=1&e=6gFP62" target="_blank">Guide de démarrage rapide</a> afin que le contenu demeure joyeux, engageant et efficace pour les élèves, tout en étant structuré pour vous. Pour les élèves de M à 2e année, FLORA s’aligne sur le programme <a href="https://curriculum.nbed.ca/learning-areas/primary-block/explore-your-world/" target="_blank">Explore Your World</a>.`,
     faq4_a:
-      "Articulate n’est plus pris en charge; vous ne pouvez donc plus télécharger les modules FLORA. Veuillez accéder aux modules à l’aide des liens sur ce site.",
+      'Articulate n’est plus pris en charge; vous ne pouvez donc plus télécharger les modules FLORA. Veuillez accéder aux <a href="#modules">modules</a> sur ce site.',
     faq3_a: `Communiquez avec la personne-ressource en <strong>FLS</strong> de votre district pour le soutien pédagogique et les ressources. Pour les enjeux techniques, veuillez créer un <strong>billet d’assistance</strong> auprès du soutien TI de votre district.`,
     faq6_a: `Bien sûr! De solides liens école-famille soutiennent l’apprentissage du français. N’hésitez pas à utiliser ce <strong><a href="https://nbed.sharepoint.com/:f:/r/sites/Test880/Shared%20Documents/1.1%20FLORA/3.%20Printable%20Resources?csf=1&web=1&e=kpK1cN" target="_blank">QR Code à emporter</a></strong> pour que les familles puissent accéder facilement à FLORA à la maison.`,
     footer_org:
